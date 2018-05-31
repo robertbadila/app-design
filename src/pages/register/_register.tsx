de74732f488a72c3e98ef6f2bce6de3bfcf30a03
Gheogheoiu Nicolae <nicolae@gheorgheoiu.com>
2018-05-31 20:21:53 +0300

    Updated readme.

--- src/pages/register/_register.tsx
+++ src/pages/register/_register.tsx
@@ -7,13 +7,10 @@ import { register } from '../../redux/auth/creators';
 export interface OwnOptionalProps {
   handleSubmit: any;
 }
-
 export interface OwnProps extends Partial<OwnOptionalProps> { }
-
 export interface StateProps {
   auth: any;
 }
-
 export interface DispatchProps {
   register: (user: any) => Function;
 }
